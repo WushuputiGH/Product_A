bc46e84a2a35cb8656204ecf29764edd0d323c8a
WushuputiGH <lanou@mac-mini-5.local>
2016-06-24 10:19:51 +0800

    Initial Commit

---
+++ Product_A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Product_A
+//
+//  Created by lanou on 16/6/24.
+//  Copyright © 2016年 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Product_A/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Product_A
+//
+//  Created by lanou on 16/6/24.
+//  Copyright © 2016年 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
